fd8d706059876ccbe253357aef9ab3921cbb415a
Florin <florinfire80@yahoo.com>
2024-04-22 19:04:48 +0300

    update

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
+const mongoose = require("mongoose");
 const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server is running. Use our API on port: 3000");
-});
+mongoose.set("strictQuery", false);
+const { DB_HOST, PORT = 5000 } = process.env;
+
+mongoose
+  .connect(DB_HOST)
+  .then(() =>
+    app.listen(PORT, () => console.log("Database connection successful"))
+  )
+  .catch((error) => {
+    console.log("Error", error.message);
+    process.exit(1);
+  });
